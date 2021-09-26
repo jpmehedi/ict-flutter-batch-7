f10576083cc90f661902bb1faa5b243bdfe88128
Mehedi Hasan <mehedi@augnitive.com>
2021-09-26 12:35:39 +0600

    Day -11

--- lib/conatact_list.dart
+++ lib/conatact_list.dart
@@ -15,6 +15,7 @@ class ContactList extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("ListView & ListTile"),),
       drawer: Drawer(
+        // elevation: 20,
         child: Column(
           children: [
             Stack(
@@ -23,35 +24,86 @@ class ContactList extends StatelessWidget {
                 Positioned(
                   left: 20,
                   top: 20,
-                  child: CircleAvatar(
-                    radius: 28,
-                    backgroundImage: AssetImage("assets/poor_man.png"),
-                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 28,
+                        backgroundImage: AssetImage("assets/poor_man.png"),
+                      ),
+                      Text(
+                        "Mehedi Hasan",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24
+                        ),
+                      ),
+                      Text(
+                      "mehedihasaninfrom@gamil.com",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14
+                        ),
+                      ),
+                    ],
+                  ), 
                 ),
-                Positioned(
-                  left: 20,
-                  top: 80,
-                  child: Text(
-                    "Mehedi Hasan",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 20,
-                  top: 110,
-                  child: Text(
-                    "mehedihasaninfrom@gamil.com",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14
-                    ),
-                  ),
-                )
+                // Positioned(
+                //   left: 20,
+                //   top: 80,
+                //   child: Text(
+                //     "Mehedi Hasan",
+                //     style: TextStyle(
+                //       color: Colors.white,
+                //       fontSize: 24
+                //     ),
+                //   ),
+                // ),
+                // Positioned(
+                //   left: 20,
+                //   top: 110,
+                //   child: Text(
+                //     "mehedihasaninfrom@gamil.com",
+                //     style: TextStyle(
+                //       color: Colors.white,
+                //       fontSize: 14
+                //     ),
+                //   ),
+                // )
               ],
+            ),
+
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.music_note),
+              title: Text("Music"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.video_call),
+              title: Text("Video"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.settings),
+              title: Text("Setting"),
+            ),
+
+            Material(
+              color: Colors.red,
+              child: InkWell(
+                onTap: (){
+                  print("Button tapped");
+                },
+                child: Container(
+                  child: Center(child: Text("Log out")),
+                  width: double.infinity,
+                  // color: Colors.black,
+                  height: 48,
+                ),
+              ),
             )
+
           ],
         ),
       ),
