56d189aa61a3d3c350693f0d5802ae071d288cb7
Mehedi Hasan <mehedi@augnitive.com>
2021-10-05 12:11:16 +0600

    Style

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:demo_project/profile.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,11 +12,12 @@ class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
 
   List<Widget> screens = [
-    Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.red,
-    ),
+    // Container(
+    //   width: double.infinity,
+    //   height: double.infinity,
+    //   color: Colors.red,
+    // ),
+    ProfileScreen(),
     Container(
       width: double.infinity,
       height: double.infinity,
@@ -26,6 +28,11 @@ class _HomeScreenState extends State<HomeScreen> {
       height: double.infinity,
       color: Colors.yellow,
     ),
+    Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.pink,
+    ),
   ];
 
   @override
@@ -34,6 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(title: Text("BottomNavigationBar"),),
       body: screens.elementAt(selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items:<BottomNavigationBarItem> [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -47,8 +55,19 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: Icon(Icons.person),
             label: "Profile"
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search"
+          ),
         ],
         currentIndex: selectedIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.black,
+        unselectedFontSize: 28,
+        selectedFontSize: 25,
+        backgroundColor: Colors.orange,
+        elevation: 20,
+        iconSize: 40,
         onTap: (int index){
           // print(index); 
           setState(() {
