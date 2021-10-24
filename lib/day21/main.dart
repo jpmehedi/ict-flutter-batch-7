881f73a025ea2fe99f2b7b43cbfc9ea123f66df4
Mehedi Hasan <mehedi@augnitive.com>
2021-10-24 12:48:22 +0600

    update

--- lib/day21/main.dart
+++ lib/day21/main.dart
@@ -15,6 +15,22 @@ void main() {
 //   final int age = 25;
 //   age = 28;
 
+  
+  
+  try{
+    int a = 24;
+    int b = 0;
+    print(a ~/ b);
+  }catch(err) {
+    
+    print("err: $err");
+    
+  }finally{
+    print("Hello World");
+  }
+    
+    
+
 
 
 
