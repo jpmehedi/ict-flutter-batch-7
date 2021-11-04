ca1fdd6826044a212a9a168d4a9542f1ccf1c138
Mehedi Hasan <mehedi@augnitive.com>
2021-11-04 11:12:40 +0600

    URL launcher

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ void main () {
       
       ),
       themeMode: ThemeMode.light,
-      initialRoute: ResponsiveScreen.path,
+      initialRoute: ScreenOne.path,
       routes: {
         ResponsiveScreen.path:(ctx)=> ResponsiveScreen(),
         ScreenOne.path : (context)=> ScreenOne(),
