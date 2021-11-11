4d3e6cba1c133ba843ab23d9caec1170e1a90ef6
Mehedi Hasan <mehedi@augnitive.com>
2021-11-11 10:38:23 +0600

    News App

---
+++ news_app/lib/screens/sports.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SportsNews extends StatelessWidget {
+  const SportsNews({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
