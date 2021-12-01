d9d75cd8da015a3881390c4138778317e23e5ac2
Mehedi Hasan <mehedi@augnitive.com>
2021-12-01 15:42:38 +0600

    Share and webview package

--- bdfoodrecipe/lib/main.dart
+++ bdfoodrecipe/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bdfoodrecipe/screens/details/details.dart';
 import 'package:bdfoodrecipe/screens/favorite/favorite.dart';
 import 'package:bdfoodrecipe/screens/home/home.dart';
+import 'package:bdfoodrecipe/screens/video/video_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         HomeScreen.path: (ctx)=> HomeScreen(),
         DetailScreen.path: (ctx)=> DetailScreen(),
         FavoriteScreen.path: (ctx)=> FavoriteScreen(),
+        VideoScreen.path: (ctx)=> VideoScreen(),
       },
     );
   }
