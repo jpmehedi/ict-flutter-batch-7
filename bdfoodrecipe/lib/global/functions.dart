21e939551b25ee42710858b35e5dc7dcd4f1c52f
Mehedi Hasan <mehedi@augnitive.com>
2022-01-20 13:04:21 +0600

    Recipe app fianl

--- bdfoodrecipe/lib/global/functions.dart
+++ bdfoodrecipe/lib/global/functions.dart
@@ -1,16 +1,19 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 SliverGridDelegateWithFixedCrossAxisCount customGridDelegate() {
-    return SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 2,
-      childAspectRatio: 0.7,
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      
-    );
+  return SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2,
+    childAspectRatio: 0.7,
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 10,
+  );
 }
 
-
-int countIngredients(List ingredients){
+int countIngredients(List ingredients) {
   return ingredients.length;
+}
+
+void launchURL(url) async {
+  if (!await launch(url)) throw 'Could not launch $url';
 }
--- bdfoodrecipe/lib/main.dart
+++ bdfoodrecipe/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:bdfoodrecipe/screen/about_us/about_us.dart';
+import 'package:bdfoodrecipe/screen/cooked.dart';
 import 'package:bdfoodrecipe/screen/details/detail.dart';
 import 'package:bdfoodrecipe/screen/favorite.dart';
 import 'package:bdfoodrecipe/screen/home/home.dart';
+import 'package:bdfoodrecipe/screen/tips/tips.dart';
 import 'package:bdfoodrecipe/screen/video/video.dart';
 import 'package:flutter/material.dart';
 
@@ -19,13 +22,14 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: HomeScreen.path,
       routes: {
-        HomeScreen.path: (ctx)=> HomeScreen(),
-        DetailScreen.path: (ctx)=>DetailScreen(),
-        YoutubeScreen.path: (ctx)=>YoutubeScreen(),
-          Favorite.path: (ctx)=>Favorite(),
-        
+        HomeScreen.path: (ctx) => HomeScreen(),
+        DetailScreen.path: (ctx) => DetailScreen(),
+        YoutubeScreen.path: (ctx) => YoutubeScreen(),
+        Favorite.path: (ctx) => Favorite(),
+        AboutUs.path: (ctx) => AboutUs(),
+        CookingTips.path: (ctx) => CookingTips(),
+        Cooked.path: (ctx) => Cooked(),
       },
     );
   }
 }
-
